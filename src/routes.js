66a32c5a2fe214eb733223479827f53b113325d6
Ricardo Montoya <ricardomontoya2002@hotmail.com>
2020-10-24 15:44:53 -0300

    initialized template api

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+
+routes.post('/users', UserController.store);
+
+routes.post('/login', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+export default routes;
